fac3c9110d404781ac5fc149983c5798add498d3
Enzo Saraiva <eggqsaraiva@gmail.com>
2023-01-04 02:52:45 -0300

    Create database

---
+++ src/base.pl
@@ -0,0 +1,175 @@
+%% Jogadores Copa
+
+%% Fatos: Nome, Selecao, Posicao, Time, Idade, Gols na Copa, Assistencias na Copa.
+
+%% Selecao Holandesa
+
+selecao(noppert,holanda,goleiro,heerenveen,28,0,0).
+selecao(dumfries,holanda,defensor,inter,26,1,2).
+selecao(vanDijk,holanda,defensor,liverpool,31,0,0).
+selecao(blind,holanda,defensor,ajax,holanda,32,1,1).
+selecao(ake,holanda,defensor,manchesterCity,27,0,0).
+selecao(roon,holanda,meia,atalanta,31,1,1).
+selecao(deJong,holanda,meia,barcelona,25,1,1).
+selecao(klaassen,holanda,meia,ajax,29,1,2).
+selecao(koopmeiners,holanda,atacante,atalanta,24,0,1).
+selecao(gakpo,holanda,atacante,psv,23,3,0).
+selecao(depay,holanda,atacante,barcelona,28,1,0).
+
+%% Selecao Inglesa
+
+selecao(pickford,inglaterra,goleiro,everton,28,0,0).
+selecao(maguire,inglaterra,defensor,manchesterUnited,29,0,1).
+selecao(stones,inglaterra,defensor,manchesterCity,28,0,0).
+selecao(shaw,inglaterra,defensor,manchesterUnited,27,0,1).
+selecao(rice,inglaterra,defensor,westHam,23,0,0).
+selecao(bellingham,inglaterra,meia,borussiaDortmund,19,1,1).
+selecao(grealish,inglaterra,meia,manchesterCity,27,1,0).
+selecao(saka,inglaterra,meia,arsenal,21,3,0).
+selecao(phoden,inglaterra,atacante,manchesterCity,22,1,2).
+selecao(kane,inglaterra,atacante,tottenham,29,2,3).
+selecao(rashford,inglaterra,atacante,manchesterUnited,25,3,0).
+
+%% Selecao Argentina
+
+selecao(martinez,argentina,goleiro,astonVilla,30,0,0).
+selecao(tagliafico,argentina,defensor,lyon,30,0,0).
+selecao(otamendi,argentina,defensor,benfica,34,,0,0).
+selecao(romero,argentina,defensor,tottenham,24,0,0).
+selecao(molina,argentina,defensor,atleticoDeMadrid,24,1,1).
+selecao(dePaul,argentina,meia,atleticoDeMadrid,28,0,0).
+selecao(enzoFernandez,argentina,meia,21,1,1).
+selecao(paredes,argentina,meia,juventus,28,0,0).
+selecao(diMaria,argentina,atacante,juventus,34,1,1).
+selecao(messi,argentina,atacante,psg,35,7,3).
+selecao(alvarez,argentina,atacante,manchesterCity,22,4,0).
+
+%% Selecao Francesa
+
+selecao(lloris,franca,goleiro,tottenham,36,0,0).
+selecao(varane,franca,defensor,manchesterUnited,29,0,0).
+selecao(kounde,franca,defensor,barcelona,28,0,0).
+selecao(theoHernandez,franca,defensor,milan,25,1,2).
+selecao(fofana,franca,meia,chelsea,23,0,0).
+selecao(rabiot,franca,meia,juventus,27,1,1).
+selecao(tchouameni,franca,meia,realMadrid,22,1,0).
+selecao(griezmann,franca,atacante,atleticoDeMadrid,31,0,3).
+selecao(dembele,franca,atacante,barcelona,25,0,2).
+selecao(mbappe,franca,atacante,psg,24,8,2).
+selecao(giroud,franca,atacante,milan,36,4,0).
+
+%% Selecao Espanhola
+
+selecao(simon,espanha,goleiro,athletic,25,0,0).
+selecao(laporte,espanha,defensor,manchesterCity,28,0,0).
+selecao(alba,espanha,defensor,barcelona,33,0,2).
+selecao(balde,espanha,defensor,barcelona,19,0,0).
+selecao(busquets,espanha,meia,barcelona,34,0,0).
+selecao(olmo,espanha,meia,leipzig,24,1,1).
+selecao(pedri,espanha,meia,barcelona,20,0,0).
+selecao(gavi,espanha,meia,barcelona,18,1,0).
+selecao(torres,espanha,atacante,barcelona,22,2,0).
+selecao(asensio,espanha,atacante,realMadrid,26,1,0).
+selecao(morata,espanha,atacante,atleticoDeMadrid,30,3,1).
+
+%% Selecao Belga
+
+selecao(courtois,belgica,goleiro,realMadrid,30,0,0).
+selecao(vertonghen,belgica,defensor,anderlecht,35,0,0).
+selecao(meunier,belgica,defensor,borussiaDortmund,31,0,0).
+selecao(castagne,belgica,defensor,leicester,34,0,0).
+selecao(tielemans,belgica,meia,leicester,24,0,0).
+selecao(witsel,belgica,meia,atleticoDeMadrid,20,0,0).
+selecao(deBruyne,belgica,meia,manchesterCity,18,0,0).
+selecao(hazard,belgica,atacante,realMadrid,31,0,0).
+selecao(trossard,belgica,atacante,brighton,28,0,0).
+selecao(lukaku,belgica,atacante,inter,29,0,0).
+selecao(batshuayi,belgica,atacante,fenerbahce,29,1,0).
+
+%% Selecao Brasileira
+
+selecao(alisson,brasil,goleiro,liverpool,30,0,0).
+selecao(danilo,brasil,defensor,juventus,31,0,0).
+selecao(marquinhos,brasil,defensor,psg,28,0,0).
+selecao(thiagoSilva,brasil,defensor,chelsea,38,0,0).
+selecao(alex,brasil,defensor,juventus,31,0,0).
+selecao(casemiro,brasil,meia,manchesterUnited,30,1,1).
+selecao(paqueta,brasil,meia,westHam,inglaterra,25,1,1).
+selecao(neymar,brasil,atacante,psg,30,2,1).
+selecao(raphinha,brasil,atacante,barcelona,26,0,0).
+selecao(viniciusJR,brasil,atacante,realMadrid,22,1,2).
+selecao(richarlison,brasil,atacante,tottenham,25,3,0).
+
+%% Selecao Portuguesa
+
+selecao(costa,portugal,goleiro,porto,23,0,0).
+selecao(pepe,portugal,defensor,porto,39,1,0).
+selecao(rubenDias,portugal,defensor,manchesterCity,25,0,0).
+selecao(guerreiro,portugal,defensor,borussiaDortmund,29,1,2).
+selecao(cancelo,portugal,defensor,manchesterCity,28,0,0).
+selecao(bernardoSilva,portugal,meia,manchesterCity,28,1,1).
+selecao(brunoFernandes,portugal,meia,manchesterUnited,28,2,3).
+selecao(vitinha,portugal,atacante,psg,22,0,0).
+selecao(joaoFelix,portugal,atacante,atleticoDeMadrid,23,1,2).
+selecao(leao,portugal,atacante,milan,23,2,0).
+selecao(cristianoRonaldo,portugal,atacante,alNassr,37,1,0).
+
+%% Distribuicao de selecoes por continente
+
+europa(holanda).
+europa(inglaterra).
+europa(franca).
+europa(espanha).
+europa(belgica).
+europa(portugal).
+europa(alemanha).
+europa(italia).
+europa(turquia).
+
+america(brasil).
+america(argentina).
+
+asia(arabia).
+
+%% Distribuicao de clubes por Pais
+
+paisClube(manchesterCity, inglaterra).
+paisClube(manchesterUnited, inglaterra).
+paisClube(arsenal, inglaterra).
+paisClube(chelsea, inglaterra).
+paisClube(tottenham, inglaterra).
+paisClube(liverpool, inglaterra).
+paisClube(brighton, inglaterra).
+paisClube(westHam, inglaterra).
+paisClube(astonVilla, inglaterra).
+paisClube(leicester, inglaterra).
+paisClube(everton, inglaterra).
+
+paisClube(barcelona,espanha).
+paisClube(realMadrid,espanha).
+paisClube(athletic,espanha).
+paisClube(atleticoDeMadrid,espanha).
+
+paisClube(milan, italia).
+paisClube(atalanta, italia).
+paisClube(inter, italia).
+paisClube(juventus, italia).
+
+paisClube(leipzig,alemanha).
+paisClube(borussiaDortmund,alemanha).
+
+paisClube(psg,franca).
+paisClube(lyon,franca).
+
+paisClube(benfica,portugal).
+paisClube(porto,portugal).
+
+paisClube(heerenveen, holanda).
+paisClube(ajax, holanda).
+paisClube(psv, holanda).
+
+paisClube(fenerbahce,turquia).
+
+paisClube(anderlecht,belgica).
+
+paisClube(alNassr,arabia).
