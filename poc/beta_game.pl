3bc05ac706f664b26f2eb4489f8b872b283441bc
Hugo Sobral <hugosobral10@gmail.com>
2023-01-05 18:40:45 -0300

    Create possible game logic
    co-authored-by: Micaella Gouveia <micaella2212@gmail.com>
    co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    co-authored-by: Enzo Gabriel <eggqsaraiva@gmail.com>

--- poc/beta_game.pl
+++ poc/beta_game.pl
@@ -11,3 +11,32 @@ posicao(richarlisson, atacante).
 selecao(richarlisson, brasil).
 camisa(richarlisson, 9).
 pergunta(richarlisson, brasileiro).
+
+atualiza_base([H|T]) :- retract(jogador(H)),
+                        atualiza_base(T).
+atualiza_base([]) :- !.
+
+verifica_vitoria(0):- write("Perdemos"), nl.
+verifica_vitoria(1):- write("Ganhamos"), nl.
+verifica_vitoria(X):- nl.
+
+fluxo_do_jogo:- % verifica vitória
+                findall(X, jogador(X), Jogadores),
+                length(Jogadores, QuantidadeDeJogadores),
+                verifica_vitoria(QuantidadeDeJogadores),
+
+                % randomiza jogador
+                random_member(JogadorAleatorio, Jogadores),
+                write(JogadorAleatorio), nl,
+
+                % randomiza pergunta
+                findall(X, pergunta(JogadorAleatorio, X), PerguntasDoJogador),
+                random_member(TemaDaPergunta, PerguntasDoJogador),
+                lidar_pergunta(TemaDaPergunta).
+
+lidar_pergunta(brasileiro) :- write("Seu jogador é brasileiro? "),
+                              read(Ans),
+                              atualizar_nacionalidade(Ans, brasil).
+
+atualizar_nacionalidade(s, Selecao):- findall(Jogador, selecao(Jogador, Sele)).
+atualizar_nacionalidade(n, Selecao):- write("Não brasileiro").
