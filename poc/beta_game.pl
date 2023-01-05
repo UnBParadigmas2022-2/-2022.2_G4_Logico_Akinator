858e3412671deced78904c73256e54675c967843
Hugo Sobral <hugosobral10@gmail.com>
2023-01-05 18:16:48 -0300

    Add initial sketch of game POC

---
+++ poc/beta_game.pl
@@ -0,0 +1,13 @@
+:- dynamic jogador/1.
+
+jogador(ney).
+posicao(ney, atacante).
+selecao(ney, brasil).
+camisa(ney, 10).
+pergunta(ney, brasileiro).
+
+jogador(richarlisson).
+posicao(richarlisson, atacante).
+selecao(richarlisson, brasil).
+camisa(richarlisson, 9).
+pergunta(richarlisson, brasileiro).
