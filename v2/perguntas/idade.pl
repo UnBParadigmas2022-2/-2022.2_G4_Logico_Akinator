ccb9f1ad3e040bb39aafd1d223a331cc5c3de600
Daniel <danielbarcelos.7@hotmail.com>
2023-01-06 01:39:06 -0300

    Create questions about age

---
+++ v2/perguntas/idade.pl
@@ -0,0 +1,114 @@
+lidar_pergunta(idade18) :-
+    write("Seu jogador possui 18 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade18).
+
+lidar_pergunta(idade19) :-
+    write("Seu jogador possui 19 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade19).
+
+lidar_pergunta(idade20) :-
+    write("Seu jogador possui 20 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade20).
+
+lidar_pergunta(idade21) :-
+    write("Seu jogador possui 21 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade21).
+
+lidar_pergunta(idade22) :-
+    write("Seu jogador possui 22 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade22).
+
+lidar_pergunta(idade23) :-
+    write("Seu jogador possui 23 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade23).
+
+lidar_pergunta(idade24) :-
+    write("Seu jogador possui 24 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade24).
+
+lidar_pergunta(idade25) :-
+    write("Seu jogador possui 25 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade25).
+
+lidar_pergunta(idade26) :-
+    write("Seu jogador possui 26 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade26).
+
+lidar_pergunta(idade27) :-
+    write("Seu jogador possui 27 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade27).
+
+lidar_pergunta(idade28) :-
+    write("Seu jogador possui 28 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade28).
+
+lidar_pergunta(idade29) :-
+    write("Seu jogador possui 29 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade29).
+
+lidar_pergunta(idade30) :-
+    write("Seu jogador possui 30 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade30).
+
+lidar_pergunta(idade31) :-
+    write("Seu jogador possui 31 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade31).
+
+lidar_pergunta(idade32) :-
+    write("Seu jogador possui 32 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade32).
+
+lidar_pergunta(idade33) :-
+    write("Seu jogador possui 33 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade33).
+
+lidar_pergunta(idade34) :-
+    write("Seu jogador possui 34 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade34).
+
+lidar_pergunta(idade35) :-
+    write("Seu jogador possui 35 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade35).
+
+lidar_pergunta(idade36) :-
+    write("Seu jogador possui 36 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade36).
+
+lidar_pergunta(idade37) :-
+    write("Seu jogador possui 37 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade37).
+
+lidar_pergunta(idade38) :-
+    write("Seu jogador possui 38 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade38).
+
+lidar_pergunta(idade39) :-
+    write("Seu jogador possui 39 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade39).
+
+lidar_pergunta(idade40) :-
+    write("Seu jogador possui 40 anos? "),
+    read(Ans),
+    atualizar_idade(Ans, idade40).
