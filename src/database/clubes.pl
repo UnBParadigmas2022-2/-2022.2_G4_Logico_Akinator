59922443ad49b6b50bd9962d19839114947fb39f
Amanda Nobre <amandapnobre@gmail.com>
2023-01-05 21:40:11 -0300

    Add clubes data base

--- src/database/clubes.pl
+++ src/database/clubes.pl
@@ -4,43 +4,123 @@
 
 %% Distribuicao de clubes por Pais
 
-paisClube(manchesterCity, inglaterra).
-paisClube(manchesterUnited, inglaterra).
-paisClube(arsenal, inglaterra).
-paisClube(chelsea, inglaterra).
-paisClube(tottenham, inglaterra).
-paisClube(liverpool, inglaterra).
-paisClube(brighton, inglaterra).
-paisClube(westHam, inglaterra).
-paisClube(astonVilla, inglaterra).
-paisClube(leicester, inglaterra).
-paisClube(everton, inglaterra).
-
-paisClube(barcelona,espanha).
-paisClube(realMadrid,espanha).
-paisClube(athletic,espanha).
-paisClube(atleticoDeMadrid,espanha).
-
-paisClube(milan, italia).
-paisClube(atalanta, italia).
-paisClube(inter, italia).
-paisClube(juventus, italia).
-
-paisClube(leipzig,alemanha).
+paisClube(augsburg,alemanha).
+paisClube(bayern,alemanha).
+paisClube(bochum,alemanha).
 paisClube(borussiaDortmund,alemanha).
-
-paisClube(psg,franca).
-paisClube(lyon,franca).
-
-paisClube(benfica,portugal).
-paisClube(porto,portugal).
-
-paisClube(heerenveen, holanda).
-paisClube(ajax, holanda).
-paisClube(psv, holanda).
-
-paisClube(fenerbahce,turquia).
-
-paisClube(anderlecht,belgica).
+paisClube(gladbach,alemanha).
+paisClube(leipzig,alemanha).
+paisClube(mainz,alemanha).
+paisClube(schalke,alemanha).
+paisClube(stuttgart,alemanha).
+paisClube(frankfurt,alemanha).
+paisClube(freiburg,alemanha).
+paisClube(werderBremen,alemanha).
 
 paisClube(alNassr,arabia).
+paisClube(alShabab,arabia).
+
+paisClube(riverplate,argentina).
+
+paisClube(salzburg,austria).
+
+paisClube(anderlecht,belgica).
+paisClube(brugge,belgica).
+
+paisClube(flamengo,brasil).
+paisClube(gremio,brasil).
+
+paisClube(montreal,canada).
+paisClube(toronto,canada).
+
+paisClube(alSadd,catar).
+
+paisClube(millonarios,colombia).
+
+paisClube(alajuelense,costaRica).
+paisClube(herediano,costaRica).
+paisClube(saprissa,costaRica).
+
+paisClube(jeonbukMotors,coreiaDoSul).
+paisClube(ulsan,coreiaDoSul).
+
+paisClube(dZagreb,croacia).
+paisClube(hajdukSplit,croacia).
+
+paisClube(celtic,escocia).
+
+paisClube(athletic,espanha).
+paisClube(atleticoDeMadrid,espanha).
+paisClube(barcelona,espanha).
+paisClube(getafe,espanha).
+paisClube(levante,espanha).
+paisClube(mallorca,espanha).
+paisClube(realMadrid,espanha).
+paisClube(sevilla,espanha).
+paisClube(valencia,espanha).
+paisClube(valladolid,espanha).
+
+paisClube(chicago,estadosUnidos).
+paisClube(cincinnati,estadosUnidos).
+paisClube(saltLake,estadosUnidos).
+
+paisClube(brest,franca).
+paisClube(lens,franca).
+paisClube(lyon,franca).
+paisClube(monaco,franca).
+paisClube(psg,franca).
+paisClube(reims,franca).
+
+paisClube(paok, grecia).
+
+paisClube(ajax, holanda).
+paisClube(heerenveen, holanda).
+paisClube(psv, holanda).
+
+paisClube(arsenal, inglaterra).
+paisClube(astonVilla, inglaterra).
+paisClube(brighton, inglaterra).
+paisClube(chelsea, inglaterra).
+paisClube(crystalPalace, inglaterra).
+paisClube(everton, inglaterra).
+paisClube(fulham, inglaterra).
+paisClube(leicester, inglaterra).
+paisClube(liverpool, inglaterra).
+paisClube(manchesterCity, inglaterra).
+paisClube(manchesterUnited, inglaterra).
+paisClube(nottinghamForest, inglaterra).
+paisClube(reading, inglaterra).
+paisClube(southampton, inglaterra).
+paisClube(sunderland, inglaterra).
+paisClube(tottenham, inglaterra).
+paisClube(westHam, inglaterra).
+paisClube(wolves, inglaterra).
+
+paisClube(atalanta, italia).
+paisClube(fiorentina, italia).
+paisClube(inter, italia).
+paisClube(juventus, italia).
+paisClube(lazio, italia).
+paisClube(milan, italia).
+paisClube(napoli, italia).
+paisClube(sampdoria, italia).
+paisClube(torino, italia).
+
+paisClube(shimizuSPulse,japao)
+paisClube(tokyo,japao)
+
+paisClube(wydad,marrocos).
+
+paisClube(benfica,portugal).
+paisClube(chaves,portugal).
+paisClube(porto,portugal).
+
+paisClube(stGallen,suica).
+
+paisClube(besiktas,turquia).
+paisClube(fenerbahce,turquia).
+paisClube(hataysport,turquia).
+
+paisClube(nacional,uruguai).
+
+paisClube(redStar,servia).
