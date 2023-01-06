1dc1047821f7c37558f02a029d20465aa5e4094b
Hugo Sobral <hugosobral10@gmail.com>
2023-01-06 00:35:21 -0300

    Add knowledge base manipulation logic

---
+++ v2/atualizacoes.pl
@@ -0,0 +1,60 @@
+:- module(atualizacoes).
+
+% --- Atualizar Selecao --- %
+atualizar_selecao(s, Selecao):- 
+    findall(Jogador, jogador(Jogador, Selecao, _, _, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_selecao(n, Selecao):- 
+    findall(Jogador, jogador(Jogador, Selecao, _, _, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
+
+
+% --- Atualizar Posição --- %
+atualizar_posicao(s, Posicao):-
+    findall(Jogador, jogador(Jogador, _, Posicao, _, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_posicao(n, Posicao):- 
+    findall(Jogador, jogador(Jogador, _, Posicao, _, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
+
+
+% --- Atualizar Time ------ %
+atualizar_time(s, Time):-
+    findall(Jogador, jogador(Jogador, _, _, Time, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_time(n, Time):-
+    findall(Jogador, jogador(Jogador, _, _, Time, _, _, _), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
+
+
+% --- Atualizar Idade ----- %
+atualizar_idade(s, Idade):-
+    findall(Jogador, jogador(Jogador, _, _, _, Idade, _, _), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_idade(n, Idade):-
+    findall(Jogador, jogador(Jogador, _, _, _, Idade, _, _), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
+
+
+% --- Atualizar Gols ------ %
+atualizar_gols(s, Gols):-
+    findall(Jogador, jogador(Jogador, _, _, _, _, Gols, _), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_gols(n, Gols):-
+    findall(Jogador, jogador(Jogador, _, _, _, _, Gols, _), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
+
+
+% --- Atualizar Assistêncis --- %
+atualizar_assists(s, Assists):-
+    findall(Jogador, jogador(Jogador, _, _, _, _, _, Assists), JogadoresDaSelecao),
+    exclui_jogadores_fora_da_lista(JogadoresDaSelecao).
+atualizar_assists(n, Assists):-
+    findall(Jogador, jogador(Jogador, _, _, _, _, _, Assists), JogadoresDaSelecao),
+    exclui_jogadores_da_lista(JogadoresDaSelecao).
+% ------------------------- %
---
+++ v2/manipulacoes.pl
@@ -0,0 +1,31 @@
+:- module(manipulacoes).
+
+% ---------- Lida com respostas negativas ---------- %
+exclui_jogadores_da_lista(ListaDeJogadores) :-
+    findall(Jogador, jogador(Jogador, _, _, _, _, _, _), TodosJogadores),
+    remove_in(TodosJogadores, ListaDeJogadores).
+
+remove_in(All, [H|T]) :- (
+    member(H, All) -> 
+        retract(jogador(H, _, _, _, _, _, _)) , remove_in(All, T) ;
+        remove_in(All, T)
+).
+remove_in(All, []) :- !.
+% -------------------------------------------------- %
+
+
+% ---------- Lida com respostas positivas ---------- %
+exclui_jogadores_fora_da_lista(ListaDeJogadores) :-
+    findall(Jogador, jogador(Jogador, _, _, _, _, _, _), TodosJogadores),
+    remove_out(TodosJogadores, ListaDeJogadores).
+
+remove_out([H|T], Lista) :- 
+    member(H, Lista),
+    remove_out(T, Lista).
+
+remove_out([H|T], Lista) :- 
+    retract(jogador(H, _, _, _, _, _, _)),
+    remove_out(T, Lista).
+
+remove_out([], Lista) :- !.
+% -------------------------------------------------- %
